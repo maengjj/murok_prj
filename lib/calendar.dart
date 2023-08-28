af1d5ea40d3f7f9f467634008277121df378c716
mjiju06 <mjiju06@gmail.com>
2023-08-28 19:27:29 +0900

    23-08-28

---
+++ lib/calendar.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_clean_calendar2/flutter_clean_calendar.dart';
+
+import 'package:murok_prj/calendar/flutter_clean_calendar.dart';
+
+class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CalendarScreenState();
+  }
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  final Map<DateTime, List<CleanCalendarEvent>> _events = {
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day): [
+      CleanCalendarEvent('Event A',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day, 10, 0),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day, 12, 0),
+          description: 'A special event',
+          color: Colors.blue.shade700),
+    ],
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day + 2):
+    [
+      CleanCalendarEvent('Event B',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 10, 0),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 12, 0),
+          color: Colors.orange),
+      CleanCalendarEvent('Event C',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.pink),
+    ],
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day + 3):
+    [
+      CleanCalendarEvent('Event B',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 10, 0),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 12, 0),
+          color: Colors.orange),
+      CleanCalendarEvent('Event C',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.pink),
+      CleanCalendarEvent('Event D',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.amber),
+      CleanCalendarEvent('Event E',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.deepOrange),
+      CleanCalendarEvent('Event F',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.green),
+      CleanCalendarEvent('Event G',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.indigo),
+      CleanCalendarEvent('Event H',
+          startTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 14, 30),
+          endTime: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 2, 17, 0),
+          color: Colors.brown),
+    ],
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _handleNewDate(DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context); // 현재 페이지를 닫고 이전 페이지로 돌아가기
+            },
+            icon: Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.white,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "캘린더",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  height: 1.2,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  // resetToToday(); // Call the function here
+                },
+                icon: Icon(
+                  Icons.today,
+                  size: 24,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          backgroundColor: Color(0xff06C09F), toolbarHeight: 80),
+      body: SafeArea(
+    child: Padding(
+    padding: const EdgeInsets.only(top: 24.0),
+        child: Calendar(
+          startOnMonday: true,
+          weekDays: const ['월', '화', '수', '목', '금', '토', '일'],
+          events: _events,
+          isExpandable: true,
+          eventDoneColor: Colors.green,
+          selectedColor: Colors.pink,
+          todayColor: Colors.blue,
+          eventColor: Colors.grey,
+          locale: 'ko_KR',
+          todayButtonText: '오늘로 가기',
+          isExpanded: true,
+          expandableDateFormat: 'yyyy MMMM dd. EEEE',
+          dayOfWeekStyle: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w800,
+            fontSize: 11,
+          ),
+        ),
+      ),
+      ),
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  void _handleNewDate(DateTime date) {
+    print('Date selected: $date');
+  }
+}
